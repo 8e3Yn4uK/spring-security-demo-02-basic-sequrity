582b6634a5b92db9936692ee6f1f4a8fb94213df
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-03-25 02:00:53 +0100

    first commit

--- src/main/java/com/springsequrity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/springsequrity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.springsequrity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,6 +24,14 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("user").password("1234").roles("USER"))
                 .withUser(users.username("manager").password("1234").roles("MANAGER"))
                 .withUser(users.username("admin").password("1234").roles("ADMIN"));
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+
 
     }
 }
